707b347678f04226e7d36807f58e5ab51c14fdb3
RizkyArifNur <rizkyarifnur@gmail.com>
2019-04-20 20:40:05 +0700

    add some documentation

--- example/controllers/hello-controller.ts
+++ example/controllers/hello-controller.ts
@@ -1,13 +1,29 @@
-import { BaseController, ErrorHandler, GET, router } from 'serd'
+import { BaseController, ErrorHandler, GET, POST, router } from 'serd'
 @router('/hello')
 export class HelloController extends BaseController {
+  /**
+   * if you not provide the path, it will automatically set by
+   * the function name `sayHello`
+   */
   @GET()
   async sayHello() {
     return 'Hello World'
   }
 
+  /**
+   * you can define your route so simple like this
+   * @param data - data that already bundled from `req.params` `req.query`
+   */
   @GET('/:name')
   async sayHelloWithQuery(data: { name: string }) {
     return `Hello ${data.name}`
+  }
+
+  @POST('/message')
+  async createGreed(data: { message: string }) {
+    if (!data.message) {
+      // if you want to throw an error just simply throw an error handler
+      throw new ErrorHandler(403, "You're not provide the message", true)
+    }
   }
 }
